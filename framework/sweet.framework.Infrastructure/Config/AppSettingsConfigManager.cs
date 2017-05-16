4c6bf268609b216ca14cffacf6307cf28da569a1
sweet <john123951@126.com>
2017-05-16 23:27:16 +0800

    modify

--- framework/sweet.framework.Infrastructure/Config/AppSettingsConfigManager.cs
+++ framework/sweet.framework.Infrastructure/Config/AppSettingsConfigManager.cs
@@ -35,6 +35,42 @@ namespace sweet.framework.Infrastructure.Config
         }
 
         /// <summary>
+        /// 在*.exe.config文件中appSettings配置节增加一对键、值对
+        /// </summary>
+        /// <param name="settingKey"></param>
+        /// <param name="newValue"></param>
+        public static void UpdateAppConfig(string settingKey, string newValue)
+        {
+            bool isModified = false;
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                if (key == settingKey)
+                {
+                    isModified = true;
+                    break;
+                }
+            }
+
+            // Open App.Config of executable
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            // You need to remove the old settings object before you can replace it
+            if (isModified)
+            {
+                config.AppSettings.Settings.Remove(settingKey);
+            }
+
+            // Add an Application Setting.
+            config.AppSettings.Settings.Add(settingKey, newValue);
+
+            // Save the changes in App.config file.
+            config.Save(ConfigurationSaveMode.Modified);
+
+            // Force a reload of a changed section.
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        /// <summary>
         /// 读取AppSettings配置节
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -42,7 +78,7 @@ namespace sweet.framework.Infrastructure.Config
         /// <param name="defValue">转换失败时的默认值</param>
         /// <param name="converter">转换函数</param>
         /// <returns></returns>
-        private static T ReadConfiguration<T>(string settingKey, T defValue, Func<String, T> converter)
+        public static T ReadConfiguration<T>(string settingKey, T defValue, Func<String, T> converter)
         {
             string settingValue = ConfigurationManager.AppSettings[settingKey];
 
